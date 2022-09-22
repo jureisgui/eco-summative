48b546ce47cc1cf1431f829115a338a938e5ca0e
jureisgui <111708095+jureisgui@users.noreply.github.com>
2022-09-22 12:11:55 +1200

    desktop finished

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,3 +1,12 @@
+document.addEventListener('DOMContentLoaded', nav)
+function nav(){
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.main-nav');
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('show')
+    })
+}
+
 const memberContainers = [...document.querySelectorAll('.members-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
